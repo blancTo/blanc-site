5e8524925c6f7d2c010ddc39bda5c8a551db3e4c
blancTo <git@blanc.to>
2023-11-03 13:04:04 +0900

    スマホ時お問い合わせ追従ボタン追加
    メール、電話、ライン

--- src/components/PageTop.js
+++ src/components/PageTop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faAngleUp } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 const PageTop = () => {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -13,7 +13,8 @@ const PageTop = () => {
   }, []);
 
   const handleScroll = () => {
-    if (window.pageYOffset > 300) { // 300はスクロール量のしきい値です。適宜変更してください。
+    if (window.pageYOffset > 300) {
+      // 300はスクロール量のしきい値です。適宜変更してください。
       setIsVisible(true);
     } else {
       setIsVisible(false);
@@ -25,12 +26,16 @@ const PageTop = () => {
   };
 
   return (
-    <button
-      className={`scroll-to-top ${isVisible ? 'visible' : ''}`}
-      onClick={scrollToTop}
-    >
-      <FontAwesomeIcon icon={faAngleUp} />
-    </button>
+    <>
+      <button className={`scroll-to-top ${isVisible ? 'visible' : ''}`} onClick={scrollToTop}>
+        <FontAwesomeIcon icon={faAngleUp} />
+      </button>
+      <div className={`scroll-line ${isVisible ? 'visible' : ''}`} onClick={scrollToTop}>
+        <a href='https://lin.ee/XRbfoOx'>
+          <img src='/images/line_icon.png' alt='友だち追加' height='45' border='0' />
+        </a>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/SlideIn.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'gatsby';
+
+const SlideIn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const scrollThreshold = 300; // スクロールしきい値（ピクセル単位）
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll > scrollThreshold) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  return (
+    <>
+      {isMobile && (
+        <>
+          <ul className={`foot-nav ${isVisible ? 'visible' : ''}`}>
+            <li className='mail'>
+              <Link to='/toiawase/'>
+                <FontAwesomeIcon icon={faEnvelope} />
+                メールで相談
+              </Link>
+            </li>
+            <li className='tel'>
+              <a href='tel:0120-781-437'>
+                <FontAwesomeIcon icon={faPhone} />
+                電話で相談
+              </a>
+            </li>
+          </ul>
+        </>
+      )}
+    </>
+  );
+};
+
+export default SlideIn;
